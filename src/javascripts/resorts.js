d88b2144229d202bdb7aab9bb37dca9ef1c09bc9
CalvinJOats <88681685+CalvinJOats@users.noreply.github.com>
2022-12-09 21:23:18 -0700

    updated image filepaths

--- src/javascripts/resorts.js
+++ src/javascripts/resorts.js
@@ -5,7 +5,7 @@ export const resorts = [{
 	webcam:'<div class="ratio ratio-16x9"><iframe src="https://www.youtube.com/embed/8WHcVXraho4?autoplay=1" title="Pebble Creek Lodge" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
 	location:"Inkom, ID",
 	description:'Pebble Creek is an alpine ski area in the western United States, located in southeastern Idaho in the Portneuf Range in the Caribou National Forest. It is east of Pocatello in eastern Bannock County, four miles (6 km) east of Inkom. <a href="https://en.wikipedia.org/wiki/Pebble_Creek,_Idaho" target="on_blank"><b>Read more...</b></a>',
-	photo: "../images/pebblecreek.png",
+	photo: "./images/pebblecreek.png",
 	seasonP: "$644",
 	fullP: "$62",
 	halfP: "$49"
@@ -17,7 +17,7 @@ export const resorts = [{
 	webcam:'<div class="ratio ratio-16x9"><iframe width="1280" height="720" src="https://www.youtube.com/embed/c4NRz9eGZqg?autoplay=1" title="Powder Mountain Webcams Live Stream" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
 	location:"Eden, UT",
 	description:'Powder Mountain is a ski resort east of Eden, Utah, stretching between Weber and Cache counties and 55 miles (89 km) from Salt Lake City International Airport. Covering 8,464 acres, Powder Mountain is the largest ski resort in the US by skiable acreage. The resort has 154 trails, nine lifts, and two terrain parks. <a href="https://en.wikipedia.org/wiki/Powder_Mountain" target="on_blank"><b>Read more...<b></a>',
-	photo: "../images/powdermountain.png",
+	photo: "./images/powdermountain.png",
 	seasonP: "$1150",
 	fullP: "$115",
 	halfP: "N/A"
@@ -29,7 +29,7 @@ export const resorts = [{
 	webcam:'<div class="ratio ratio-16x9"><iframe src="https://www.youtube.com/embed/g7fWaol3P1g?autoplay=1" title="Grand Targhee Dreamcatcher - SeeJH.com" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
 	location:"Alta, WY",
 	description:'Grand Targhee Resort is a ski resort in the western United States, located in western Wyoming in the Caribou-Targhee National Forest, near Alta, the closest town to the resort. It is 42 miles (68 km) northwest of Jackson and is accessible only from the west, through Driggs, Idaho. <a href="https://en.wikipedia.org/wiki/Grand_Targhee_Resort" target="on_blank"><b>Read more...</b></a>',
-	photo: "../images/grandtarghee.png",
+	photo: "./images/grandtarghee.png",
 	seasonP: "$1449",
 	fullP: "$120",
 	halfP: "$96"
@@ -41,7 +41,7 @@ export const resorts = [{
 	webcam:'<div class="ratio ratio-16x9"><iframe src="https://www.youtube.com/embed/uDNfHyb2LL8?autoplay=1" title="Nordic Valley Live Stream" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
 	location:"Eden, UT",
 	description:'Nordic Valley (formally titled Wolf Creek Utah Ski Resort) is a small local ski area in Nordic Valley, Utah. The area was known as Nordic Valley until June 29, 2005, when it was acquired by the nearby Wolf Creek Golf Resort. The resort is known for its inexpensive tickets and as a good place to take children for their first introduction to skiing or snowboarding. Wolf Mountain has been described as having "the least expensive skiing and riding in Utah". <a href="https://en.wikipedia.org/wiki/Wolf_Mountain" target="on_blank"><b>Read more...</b></a>',
-	photo: "../images/nordicvalley.png",
+	photo: "./images/nordicvalley.png",
 	seasonP: "$499+",
 	fullP: "<$60",
 	halfP: "N/A"
@@ -53,7 +53,7 @@ export const resorts = [{
 	webcam:'<div class="ratio ratio-16x9"><iframe src="https://www.youtube.com/embed/l2X9DlRzX54?autoplay=1" title="Apex Express and Sunrise Lift Lines" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
 	location:"Solitude, UT",
 	description:'Solitude Mountain Resort is a ski resort located in the Big Cottonwood Canyon of the Wasatch Mountains, thirty miles southeast of Salt Lake City, Utah. With 66 trails, 1,200 acres and 2,047 feet (624 m) vertical, Solitude is one of the smaller ski resorts near Salt Lake City, along with its neighbor Brighton. <a href="https://en.wikipedia.org/wiki/Solitude_Mountain_Resort" target="on_blank"><b>Read more...</b></a>',
-	photo: "../images/solitude.png",
+	photo: "./images/solitude.png",
 	seasonP: "Ikon Pass",
 	fullP: "$115+",
 	halfP: "N/A"
